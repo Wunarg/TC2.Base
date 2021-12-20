42bb26db342034cbd0685d09af9736be69663ddd
Wunarg <warl.durm@gmail.com>
2021-12-20 11:00:34 +0100

    Reanimatable flesh modifier

--- Content/Data/Modifications.Organic.cs
+++ Content/Data/Modifications.Organic.cs
@@ -6,7 +6,24 @@ namespace TC2.Base
 	{
 		private static void RegisterOrganicModifications(ref List<Modification.Definition> definitions)
 		{
+			definitions.Add(Modification.Definition.New<Organic.Data>
+			(
+				identifier: "organic.healfromdeath",
+				category: "Organic",
+				name: "Reanimatable Flesh",
+				description: "Will come back to live if healed enough",
 			
+				can_add: static (ref Modification.Context context, in Organic.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					return context.GetComponent<HealFromDeath.Data>().IsNull();
+				},
+
+				apply_0: static (ref Modification.Context context, ref Organic.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					ref var healFromDeath = ref context.GetOrAddComponent<HealFromDeath.Data>();
+					context.requirements_new.Add(Crafting.Requirement.Resource("salt.life", 15.00f));
+				}
+			));
 		}
 	}
 }
---
+++ Content/Data/Modifier Components/HealFromDeath.cs
@@ -0,0 +1,28 @@
+﻿
+namespace TC2.Base.Components
+{
+	public static partial class HealFromDeath
+	{
+
+		[IComponent.Data(Net.SendType.Reliable)]
+		public partial struct Data: IComponent
+		{
+
+		}
+
+		[ISystem.Modified<Health.Data>(ISystem.Mode.Single)]
+		[Exclude<Player.Data>(Source.Modifier.Shared)]
+		[HasTag("dead", true, Source.Modifier.Owned)]
+		public static void OnModified(ISystem.Info info, Entity entity,
+		[Source.Owned] ref Organic.Data orgData, [Source.Owned] in HealFromDeath.Data data, 
+		[Source.Owned] ref Organic.State orgState, [Source.Owned] in Health.Data health)
+		{
+			if (health.primary > 0.30f && health.secondary > 0.30f)
+			{
+				orgState.consciousness_shared_new += 0.5f;
+				orgState.consciousness_shared += 0.5f;
+				entity.SyncComponent<Organic.State>(ref orgState);
+			}
+		}
+	}
+}
