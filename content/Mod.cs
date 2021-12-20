57e2aee27ded9ce2d51291223548c39454c3f3fc
Wunarg <warl.durm@gmail.com>
2021-12-20 10:22:46 +0100

    Organic Mods File

--- content/Mod.cs
+++ content/Mod.cs
@@ -11,6 +11,7 @@ namespace TC2.Base
 			Modification.OnInitialize += RegisterDrillModifications;
 			Modification.OnInitialize += RegisterMedkitModifications;
 			Modification.OnInitialize += RegisterExplosiveModifications;
+			Modification.OnInitialize += RegisterOrganicModifications;
 		}
 
 		protected override void OnInitialize(ModContext context)
